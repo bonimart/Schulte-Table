77234c3144a2405520813c666c6a6176dacc96f6
Martin Beneš <martin.benes@stratox.com>
2024-09-19 16:16:58 +0200

    change appearance for desktop

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
-use bevy::{
-    prelude::*,
-    window::WindowMode,
-};
+use bevy::prelude::*;
 use schulte_table::GamePlugin;
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        resizable: false,
-                        mode: WindowMode::BorderlessFullscreen,
-                        ..default()
-                    }),
-                    ..default()
-                }),
+            DefaultPlugins,
             GamePlugin::default(),
         ))
         .run();
